6ca762c371a3e6e8fe16e16b3c350a0cfad5e21e
CarlosM-08 <carlos_murillo1@unitec.edu>
2026-08-18 11:22:55 -0600

    Actualizacion ConversordeUnidadesFisicas

--- ConversordeUnidadesFisicas/src/conversordeunidadesfisicas/ConversordeUnidadesFisicas.java
+++ ConversordeUnidadesFisicas/src/conversordeunidadesfisicas/ConversordeUnidadesFisicas.java
@@ -27,11 +27,11 @@ public class ConversordeUnidadesFisicas {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        
+
         //Declaracion de variables
         int seleccion = 0;
         double metros = 0, kilogramos = 0, centimetros = 0, pies = 0, libras = 0, pulgadas = 0;
-        
+
         do {
             System.out.println("=====CONVERSION DE MEDIDAS DE LONGITUD Y PESO=====");
             System.out.println("""
@@ -42,60 +42,60 @@ public class ConversordeUnidadesFisicas {
                                3. Centimetros a Pulgadas
                                4. Salir                               
                                """);
-            
+
             System.out.print("Ingrese su respuesta: ");
             seleccion = input.nextInt();
-            
+
             switch (seleccion) {
                 case 1:
                     System.out.println("");
-                    System.out.println("===Conversion de metros a pies===");                    
+                    System.out.println("===Conversion de metros a pies===");
                     System.out.print("Ingrese la cantidad de metros: ");
-                    metros = input.nextDouble();                      
+                    metros = input.nextDouble();
                     while (metros < 0) {
-                        System.out.println("El dato ingresado debe ser mayor que cero"); 
+                        System.out.println("El dato ingresado debe ser mayor que cero");
                         System.out.print("Ingrese la cantidad de metros: ");
-                        metros = input.nextDouble();                                                
-                    }                    
+                        metros = input.nextDouble();
+                    }
                     pies = metros * 3.28;
-                    System.out.printf("%.2f metros equivale a %.2f pies\n", metros, pies); 
-                    System.out.println("");                   
+                    System.out.printf("%.2f metros equivale a %.2f pies\n", metros, pies);
+                    System.out.println("");
                     break;
                 case 2:
                     System.out.println("");
-                    System.out.println("===Conversion de kilogramos a libras===");                    
+                    System.out.println("===Conversion de kilogramos a libras===");
                     System.out.print("Ingrese la cantidad de kilogramos: ");
-                    kilogramos = input.nextDouble();                      
+                    kilogramos = input.nextDouble();
                     while (kilogramos < 0) {
-                        System.out.println("El dato ingresado debe ser mayor que cero"); 
+                        System.out.println("El dato ingresado debe ser mayor que cero");
                         System.out.print("Ingrese la cantidad de kilogramos: ");
-                        kilogramos = input.nextDouble();                                                
-                    }                    
+                        kilogramos = input.nextDouble();
+                    }
                     libras = kilogramos / 0.454;
-                    System.out.printf("%.2f kilogramos equivale a %.2f libras\n", kilogramos, libras); 
-                    System.out.println("");                    
+                    System.out.printf("%.2f kilogramos equivale a %.2f libras\n", kilogramos, libras);
+                    System.out.println("");
                     break;
                 case 3:
                     System.out.println("");
-                    System.out.println("===Conversion de centimetros a pulgadas===");                    
+                    System.out.println("===Conversion de centimetros a pulgadas===");
                     System.out.print("Ingrese la cantidad de centimetros: ");
-                    centimetros = input.nextDouble();                      
+                    centimetros = input.nextDouble();
                     while (centimetros < 0) {
-                        System.out.println("El dato ingresado debe ser mayor que cero"); 
+                        System.out.println("El dato ingresado debe ser mayor que cero");
                         System.out.print("Ingrese la cantidad de centimetros: ");
-                        centimetros = input.nextDouble();                                                
-                    }                    
+                        centimetros = input.nextDouble();
+                    }
                     pulgadas = centimetros * 0.394;
-                    System.out.printf("%.2f centimetros equivale a %.2f pulgadas\n", centimetros, pulgadas); 
-                    System.out.println("");                    
+                    System.out.printf("%.2f centimetros equivale a %.2f pulgadas\n", centimetros, pulgadas);
+                    System.out.println("");
                     break;
                 default:
                     break;
             }//Fin switch
-            
+
         } while (seleccion != 4);
         System.out.println("El programa ha finalizado");
-        
+
     }//Fin Main
-    
+
 }//FIna Class
